ff55edb9197e95cf2e77f25f35691313ec14743c
Rene Hexel <r.hexel@griffith.edu.au>
2023-08-19 16:06:49 +1000

    More arrangement code wip.

--- Sources/FSM/CBinding+ArrangementCode.swift
+++ Sources/FSM/CBinding+ArrangementCode.swift
@@ -31,7 +31,7 @@ public func cArrangementInterface(for instances: [Instance], named name: String,
         Code.bracedBlock {
             "union"
             Code.bracedBlock {
-                "struct LLFSMachine *instances[\(instances.count)];"
+                "struct LLFSMachine *machines[\(instances.count)];"
                 "struct"
                 Code.bracedBlock {
                     Code.forEach(instances) { instance in
@@ -40,6 +40,11 @@ public func cArrangementInterface(for instances: [Instance], named name: String,
                 } + ";"
             } + ";"
         } + ";"
+        ""
+        "/// Initialise the \(name) LLFSM arrangement."
+        "///"
+        "/// - Parameter arrangement: The machine arrangement to initialise."
+        "void arrangement_" + name + "_init(struct Arrangement_" + name + " * const arrangement);"
     }
 }
 
@@ -63,5 +68,52 @@ public func cArrangementCode(for instances: [Instance], named name: String, isSu
     #endif
 
     """ + .block {
+        ""
+        "/// Initialise the \(name) LLFSM arrangement."
+        "///"
+        "/// - Parameter arrangement: The machine arrangement to initialise."
+        "void arrangement_" + name + "_init(struct Arrangement_" + name + " * const arrangement)"
+        Code.bracedBlock {
+        }
+        ""
+        "/// Run a ringlet of the \(name) LLFSM arrangement."
+        "///"
+        "/// - Parameter arrangement: The machine arrangement to initialise."
+        "void arrangement_" + name + "_execute_once(struct Arrangement_" + name + " * const arrangement)"
+        Code.bracedBlock {
+            Code.forEach(instances) { instance in
+                "struct Machine_\(instance.url.deletingPathExtension().lastPathComponent) *\(instance.name);"
+            }
+        }
+    }
+}
+
+/// Return the implementation for a C-language LLFSM arrangement.
+///
+/// - Parameters:
+///   - instances: The instances to arrange.
+///   - name: The name of the arrangement
+///   - isSuspensible: Indicates whether code for suspensible machines should be generated.
+/// - Returns: The LLFSM arrangement implementation code.
+public func cArrangementMachineCode(for instances: [Instance], named name: String, isSuspensible: Bool) -> String {
+    """
+    //
+    // Machine_Common.c
+    //
+    // Automatically created using fsmconvert -- do not change manually!
+    //
+    #include \"Machine_Common.h\"
+    #ifndef NULL
+    #define NULL ((void*)0)
+    #endif
+
+    """ + .block {
+        ""
+        "/// Run a ringlet of a C-language LLFSM."
+        "///"
+        "/// - Parameter machine: The machine arrangement to initialise."
+        "void llfsm_execute_once(struct LLFSMArrangement * const arrangement, struct LLFSMachine * const machine)"
+        Code.bracedBlock {
+        }
     }
 }
